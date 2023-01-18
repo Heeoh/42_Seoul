e10b745fd14638377daa6ac8f0c0f2aeed3c000f
Heeoh Son <heson@c2r8s1.42seoul.kr>
2023-01-18 20:52:41 +0900

    23.01.18 - implement mandatory

---
+++ mini_talk/client.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heson <heson@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 15:34:17 by heson             #+#    #+#             */
+/*   Updated: 2023/01/18 20:46:16 by heson            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>			// signal
+#include <string.h>
+#include <unistd.h>
+
+
+void convert2binary_n_send(int server_pid, char ch)
+{
+	for (int i=(1 << 7); i > 0; i >>= 1) {
+		if ((ch & i) == 0) {
+			// printf("send SIGSUR1\n");
+			kill(server_pid, SIGUSR1);
+		}
+		else {
+			// printf("send SIGSUR2\n");
+			kill(server_pid, SIGUSR2);
+		}
+		usleep(100);
+	}
+}
+
+int main(int ac, char *av[]) // ac=3. av[1]=server pid, av[2]=str
+{
+	int server_pid;
+	int str_len;
+
+	// communicate to server, send the str as parameter
+	if (ac == 3) {
+		server_pid = atoi(av[1]);  
+		printf("client is connecting to %d ...\n", server_pid);
+		str_len = strlen(av[2]);
+		for (int i=0; i<str_len; i++)
+			convert2binary_n_send(server_pid, av[2][i]);
+		convert2binary_n_send(server_pid, '\n');
+
+	}
+	
+
+}
