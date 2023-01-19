a3f7bde8f5905ea6249e9ffa7ea6b2ee02202237
heeoh <hio4662@naver.com>
2023-01-19 23:17:39 +0900

    create Makefile

---
+++ mini_talk/headers/minitalk.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heson <heson@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 01:12:04 by heson             #+#    #+#             */
+/*   Updated: 2023/01/19 02:23:35 by heson            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>			// signal
+// #include <string.h>
+#include <unistd.h>
+#include "../library/libft/libft.h"
+#include "../library/printf/headers/ft_printf.h"
