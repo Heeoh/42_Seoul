e4744afe94419bc5643bd98f56f498dcc2a36d58
heeoh <hio4662@naver.com>
2023-01-20 03:40:13 +0900

    norm, error handling Makefile

--- mini_talk/client.c
+++ mini_talk/client.c
@@ -6,41 +6,42 @@
 /*   By: heson <heson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:34:17 by heson             #+#    #+#             */
-/*   Updated: 2023/01/20 01:59:05 by heson            ###   ########.fr       */
+/*   Updated: 2023/01/20 03:38:29 by heson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/minitalk.h"
 
-void convert2binary_n_send(int server_pid, char ch)
+void	convert2binary_n_send(int server_pid, char ch)
 {
-	for (int i=(1 << 7); i > 0; i >>= 1) {
-		if ((ch & i) == 0) {
-			// printf("send SIGSUR1\n");
+	int	i;
+
+	i = (1 << 8);
+	while (i > 0)
+	{
+		if ((ch & i) == 0)
 			kill(server_pid, SIGUSR1);
-		}
-		else {
-			// printf("send SIGSUR2\n");
+		else
 			kill(server_pid, SIGUSR2);
-		}
 		usleep(100);
+		i >>= 1;
 	}
 }
 
-int main(int ac, char *av[]) // ac=3. av[1]=server pid, av[2]=str
+int	main(int ac, char *av[])
 {
-	int server_pid;
-	int str_len;
+	int	server_pid;
+	int	str_len;
+	int	i;
 
-	// communicate to server, send the str as parameter
-	if (ac == 3) {
-		server_pid = atoi(av[1]);  
+	if (ac == 3)
+	{
+		server_pid = atoi(av[1]);
 		ft_printf("client is connecting to %d ...\n", server_pid);
 		str_len = ft_strlen(av[2]);
-		for (int i=0; i<str_len; i++)
-			convert2binary_n_send(server_pid, av[2][i]);
+		i = 0;
+		while (i < str_len)
+			convert2binary_n_send(server_pid, av[2][i++]);
 		convert2binary_n_send(server_pid, '\n');
-
 	}
-	
 }
--- mini_talk/server.c
+++ mini_talk/server.c
@@ -6,45 +6,39 @@
 /*   By: heson <heson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:34:15 by heson             #+#    #+#             */
-/*   Updated: 2023/01/20 02:46:40 by heson            ###   ########.fr       */
+/*   Updated: 2023/01/20 03:39:49 by heson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/minitalk.h"
 
-void signal_handler (int sig)
+void	signal_handler(int sig)
 {
-	static int cnt;
-	static char my_char;
-	int bit;
+	static int	cnt;
+	static char	my_char;
+	int			bit;
 
-	
 	bit = 1;
 	if (sig == SIGUSR1)
 		bit = 0;
 	my_char = (my_char << 1) | bit;
 	cnt++;
-	// printf("client is connected %d, %d\n", cnt, bit);
-	if (cnt >= 8) {
+	if (cnt >= 8)
+	{
 		if (ft_printf("%c", my_char))
 			my_char = 0;
 		cnt = 0;
 	}
 }
 
-
-int main(void)
+int	main(void)
 {
 	pid_t	pid;
 
 	pid = getpid();
 	ft_printf("%d\n", pid);
-
-	// if get str from client, print the str
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
-
-	while (1) {
+	while (1)
 		pause();
-	}
 }
