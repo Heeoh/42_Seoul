79790cc6bf5b8a3ae4749c0ce7f26c5f4c0f7277
Heeoh Son <heson@cx1r4s4.42seoul.kr>
2023-02-12 17:51:17 +0900

    quick sort ok

--- push_swap/sources/sorting.c
+++ push_swap/sources/sorting.c
@@ -6,80 +6,13 @@
 /*   By: heson <heson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 13:17:17 by heson             #+#    #+#             */
-/*   Updated: 2023/02/12 16:11:12 by heson            ###   ########.fr       */
+/*   Updated: 2023/02/12 17:51:04 by heson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/stack.h"
 #include "../headers/operations.h"
 #include "../headers/push_swap.h"
-
-
-//-------------------------- quick sorting -----------------------------//
-
-// void	b_to_a(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted);
-// void	a_to_b(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
-// {
-// 	int	tmp;
-// 	int	pivot;
-
-// 	if (e - s < 2)
-// 	{
-// 		if (e - s == 1)
-// 		{
-// 			tmp = get_top(*stk_a);
-// 			do_operation(SA, stk_a, stk_b);
-// 			if (tmp < get_top(*stk_a))
-// 				do_operation(SA, stk_a, stk_b);
-// 		}
-// 		return ;
-// 	}
-// 	pivot = sorted[(s + e) / 2];
-// 	tmp = get_stk_size(*stk_a);
-// 	while (tmp--)
-// 	{
-// 		if (get_top(*stk_a) >= pivot)
-// 			do_operation(RA, stk_a, stk_b);
-// 		else
-// 			do_operation(PB, stk_a, stk_b);
-// 	}
-// 	a_to_b(stk_a, stk_b, (s + e) / 2, e, sorted);
-// 	b_to_a(stk_a, stk_b, s, (s + e) / 2 - 1, sorted);
-// }
-
-// void	b_to_a(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
-// {
-// 	int	tmp;
-// 	int	pivot;
-
-// 	if (e - s < 2)
-// 	{
-// 		if (e - s == 1)
-// 		{
-// 			tmp = get_top(*stk_b);
-// 			do_operation(SB, stk_a, stk_b);
-// 			if (tmp > get_top(*stk_b))
-// 				do_operation(SB, stk_a, stk_b);
-// 		}
-// 		tmp = e - s + 1;
-// 		while (tmp--)
-// 			do_operation(PA, stk_a, stk_b);
-// 		return ;
-// 	}
-// 	pivot = sorted[(s + e) / 2];
-// 	tmp = get_stk_size(*stk_b);
-// 	while (tmp--)
-// 	{
-// 		if (get_top(*stk_b) <= pivot)
-// 			do_operation(RB, stk_a, stk_b);
-// 		else
-// 			do_operation(PA, stk_a, stk_b);
-// 	}
-// 	a_to_b(stk_a, stk_b, (s + e) / 2 + 1, e, sorted);
-// 	b_to_a(stk_a, stk_b, s, (s + e) / 2, sorted);	
-// }
-
-//----------------------- dual quick sorting --------------------------//
 
 void	sort_2nums(t_stack *stk_a, t_stack *stk_b, int top)
 {
@@ -90,11 +23,11 @@ void	sort_2nums(t_stack *stk_a, t_stack *stk_b, int top)
 void	sort_3nums(t_stack *stk_a, t_stack *stk_b, int bottom, int top)
 {
 
-	if (stk_a->memory[stk_a->top - 2] == bottom && get_top(*stk_a) == top) // 321
+	if (stk_a->memory[stk_a->top - 2] == bottom && get_top(*stk_a) == top)
 		return ;
-	else if (stk_a->memory[stk_a->top - 2] == bottom && get_top(*stk_a) != top) // 312
+	else if (stk_a->memory[stk_a->top - 2] == bottom && get_top(*stk_a) != top)
 		do_operation(SA, stk_a, stk_b);
-	else if (stk_a->memory[stk_a->top - 2] == top && get_top(*stk_a) == bottom) // 123
+	else if (stk_a->memory[stk_a->top - 2] == top && get_top(*stk_a) == bottom)
 	{
 		do_operation(SA, stk_a, stk_b);
 		do_operation(RRA, stk_a, stk_b);
@@ -110,154 +43,232 @@ void	sort_3nums(t_stack *stk_a, t_stack *stk_b, int bottom, int top)
 	}
 }
 
-void	partition_a(t_stack *stk_a, t_stack *stk_b, int pivot1, int pivot2)
-{
-	int	cnt;
+//-------------------------- quick sorting -----------------------------//
 
-	cnt = get_stk_size(*stk_a);
-	while (cnt--)
-	{
-		if (get_top(*stk_a) >= pivot2)
-			do_operation(RA, stk_a, stk_b);
-		else
-		{
-			do_operation(PB, stk_a, stk_b);
-			if (get_top(*stk_b) >= pivot1)
-				do_operation(RB, stk_a, stk_b);	
-		}
-	}
-	while (stk_b->memory[0] >= pivot1)
-		do_operation(RRB, stk_a, stk_b);
-}
+// void	a_to_b(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
+// {
+// 	int	cnt;
+// 	int mid;
+// 	int	pivot;
 
-void	a_to_b(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
-{
-	int	pivot1;
-	int pivot2;
+// 	if (e - s < 2)
+// 	{
+// 		if (e - s == 1)
+// 			sort_2nums(stk_a, stk_b, sorted[s]);
+// 		return ;
+// 	}
+// 	mid = (s + e) / 2;
+// 	pivot = sorted[mid];
+// 	cnt = e - s + 1;
+// 	while (cnt--)
+// 	{
+// 		if (get_top(*stk_a) > pivot)
+// 			do_operation(RA, stk_a, stk_b);
+// 		else
+// 			do_operation(PB, stk_a, stk_b);
+// 	}
+// 	cnt = e - mid;
+// 	while (cnt--)
+// 		do_operation(RRA, stk_a, stk_b);
+// 	a_to_b(stk_a, stk_b, mid + 1, e, sorted);
+// 	b_to_a(stk_a, stk_b, s, mid, sorted);
+// }
 
-	if (e - s == 2)
-	{
-		// == 3 -> pb ra pa sa rra & top != 1 -> sa
-		// == 2 -> prev == 3 -> pb sa pa & sa
-		// == 1 -> prev == 3 -> ra sa rra
-		if (get_top(*stk_a) == sorted[e])
-		{
-			do_operation(PB, stk_a, stk_b);
-			do_operation(RA, stk_a, stk_b);
-			do_operation(PA, stk_a, stk_b);
-			do_operation(SA, stk_a, stk_b);
-			do_operation(RRA, stk_a, stk_b);
-			if (get_top(*stk_a) != sorted[s])
-				do_operation(SA, stk_a, stk_b);
-		}
-		else if (get_top(*stk_a) == sorted[s] 
-				&& stk_a->memory[stk_a->top - 1] == sorted[e])
-		{
-			do_operation(RA, stk_a, stk_b);
-			do_operation(SA, stk_a, stk_b);
-			do_operation(RRA, stk_a, stk_b);
-		}
-		else
-		{
-			if (stk_a->memory[stk_a->top - 1] == sorted[e])
-			{
-				do_operation(PB, stk_a, stk_b);
-				do_operation(SA, stk_a, stk_b);
-				do_operation(PA, stk_a, stk_b);
-			}
-			do_operation(SA, stk_a, stk_b);
-		}
-		return;
-	}
-	else if (e - s < 2)
-	{
-		if (e != s)
-			sort_2nums(stk_a, stk_b, sorted[s]);
-		return;
-	}
-	pivot1 = s + ((e - s) / 3);
-	pivot2 = e - ((e - s) / 3);
-	partition_a(stk_a, stk_b, sorted[pivot1], sorted[pivot2]);
-	a_to_b(stk_a, stk_b, pivot2, e, sorted);
-	b_to_a(stk_a, stk_b, pivot1, pivot2 - 1, sorted);
-	b_to_a(stk_a, stk_b, s, pivot1 - 1, sorted);	
-}
+// void	b_to_a(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
+// {
+// 	int	cnt;
+// 	int mid;
+// 	int	pivot;
 
-void	partition_b(t_stack *stk_a, t_stack *stk_b, int pivot1, int pivot2)
-{
-	int	cnt;
+// 	if (e - s < 2)
+// 	{
+// 		cnt = e - s + 1;
+// 		while (cnt--)
+// 			do_operation(PA, stk_a, stk_b);
+// 		a_to_b(stk_a, stk_b, s, e, sorted);
+// 		return ;
+// 	}
+// 	mid = (s + e) / 2;
+// 	pivot = sorted[mid];
+// 	cnt = e - s + 1;
+// 	while (cnt--)
+// 	{
+// 		if (get_top(*stk_b) <= pivot)
+// 			do_operation(RB, stk_a, stk_b);
+// 		else
+// 			do_operation(PA, stk_a, stk_b);
+// 	}
+// 	cnt = mid - s + 1;
+// 	while (cnt--)
+// 		do_operation(RRB, stk_a, stk_b);
+// 	a_to_b(stk_a, stk_b, mid + 1, e, sorted);
+// 	b_to_a(stk_a, stk_b, s, mid, sorted);	
+// }
 
-	cnt = get_stk_size(*stk_b);
-	while (cnt--)
-	{
-		if (get_top(*stk_b) < pivot1)
-			do_operation(RB, stk_a, stk_b);
-		else
-		{
-			do_operation(PA, stk_a, stk_b);
-			if (get_top(*stk_a) < pivot2)
-				do_operation(RA, stk_a, stk_b);	
-		}
-	}
-	while (stk_a->memory[0] < pivot2)
-		do_operation(RRA, stk_a, stk_b);
-}
+//----------------------- dual quick sorting --------------------------//
 
-void	b_to_a(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
-{
-	int cnt;
-	int	pivot1;
-	int pivot2;
 
-	if (e - s == 2)
-	{
-		// == 3 -> pa pa pa & top_a != 1 -> sa
-		// == 2 -> pa & top_b != 3 -> sb & pa sa pa
-		// == 1 -> rb pa pa & top_a == 3 -> sa & rrb pa
-		if (get_top(*stk_b) == sorted[e])
-		{
-			do_operation(PA, stk_a, stk_b);
-			do_operation(PA, stk_a, stk_b);
-			do_operation(PA, stk_a, stk_b);
-			if (get_top(*stk_a) != sorted[s])
-				do_operation(SA, stk_a, stk_b);
-		}
-		else if (get_top(*stk_b) == sorted[s])
-		{
-			do_operation(RB, stk_a, stk_b);
-			do_operation(PA, stk_a, stk_b);
-			do_operation(PA, stk_a, stk_b);
-			if (get_top(*stk_a) == sorted[e])
-				do_operation(SA, stk_a, stk_b);
-			do_operation(RRB, stk_a, stk_b);
-			do_operation(PA, stk_a, stk_b);
-		}
-		else
-		{
-			do_operation(PA, stk_a, stk_b);
-			if (get_top(*stk_b) != sorted[e])
-				do_operation(SB, stk_a, stk_b);
-			do_operation(PA, stk_a, stk_b);
-			do_operation(SA, stk_a, stk_b);
-			do_operation(PA, stk_a, stk_b);
-		}
-		return ;
-	}
-	else if (e - s < 2)
-	{
-		cnt = e - s + 1;
-		while (cnt--)
-			do_operation(PA, stk_a, stk_b);
-		a_to_b(stk_a, stk_b, s, e, sorted);
-		return ;
-	}
-	pivot1 = s + (e - s) / 3;
-	pivot2 = e - (e - s) / 3;
-	partition_b(stk_a, stk_b, sorted[pivot1], sorted[pivot2]);
-	a_to_b(stk_a, stk_b, pivot1, pivot2 - 1, sorted);
-	a_to_b(stk_a, stk_b, pivot2, e, sorted);
-	b_to_a(stk_a, stk_b, s, pivot1 - 1, sorted);
-}
+
+
+
+// void	partition_a(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
+// {
+// 	int	cnt;
+// 	int pivot1;
+// 	int pivot2;
+
+// 	pivot1 = s + (e - s) / 3;
+// 	pivot2 = e - (e - s) / 3;
+// 	cnt = e - s + 1;
+// 	while (cnt--)
+// 	{
+// 		if (get_top(*stk_a) >= sorted[pivot2])
+// 			do_operation(RA, stk_a, stk_b);
+// 		else
+// 		{
+// 			do_operation(PB, stk_a, stk_b);
+// 			if (get_top(*stk_b) >= sorted[pivot1])
+// 				do_operation(RB, stk_a, stk_b);	
+// 		}
+// 	}
+// 	while (stk_b->memory[0] >= sorted[pivot1])
+// 		do_operation(RRB, stk_a, stk_b);
+// }
+
+// void	a_to_b(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
+// {
+// 	int	pivot1;
+// 	int pivot2;
+
+// 	if (e - s == 2)
+// 	{
+// 		if (get_top(*stk_a) == sorted[e])
+// 		{
+// 			do_operation(PB, stk_a, stk_b);
+// 			do_operation(RA, stk_a, stk_b);
+// 			do_operation(PA, stk_a, stk_b);
+// 			do_operation(SA, stk_a, stk_b);
+// 			do_operation(RRA, stk_a, stk_b);
+// 			if (get_top(*stk_a) != sorted[s])
+// 				do_operation(SA, stk_a, stk_b);
+// 		}
+// 		else if (get_top(*stk_a) == sorted[s])
+// 		{
+// 			if (stk_a->memory[stk_a->top - 1] != sorted[e])
+// 				return ;
+// 			do_operation(RA, stk_a, stk_b);
+// 			do_operation(SA, stk_a, stk_b);
+// 			do_operation(RRA, stk_a, stk_b);
+// 		}
+// 		else
+// 		{
+// 			if (stk_a->memory[stk_a->top - 1] == sorted[e])
+// 			{
+// 				do_operation(PB, stk_a, stk_b);
+// 				do_operation(SA, stk_a, stk_b);
+// 				do_operation(PA, stk_a, stk_b);
+// 			}
+// 			do_operation(SA, stk_a, stk_b);
+// 		}
+// 		return;
+// 	}
+// 	else if (e - s < 2)
+// 	{
+// 		if (e != s)
+// 			sort_2nums(stk_a, stk_b, sorted[s]);
+// 		return;
+// 	}
+// 	pivot1 = s + ((e - s) / 3);
+// 	pivot2 = e - ((e - s) / 3);
+// 	partition_a(stk_a, stk_b, s, e, sorted);
+// 	a_to_b(stk_a, stk_b, pivot2, e, sorted); 			// L
+// 	b_to_a(stk_a, stk_b, pivot1, pivot2 - 1, sorted);	// M
+// 	b_to_a(stk_a, stk_b, s, pivot1 - 1, sorted);		// S
+// }
+
+// void	partition_b(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
+// {
+// 	int	cnt;
+// 	int pivot1;
+// 	int pivot2;
+
+// 	pivot1 = s + (e - s) / 3;
+// 	pivot2 = e - (e - s) / 3;
+// 	cnt = e - s + 1;
+// 	while (cnt--)
+// 	{
+// 		if (get_top(*stk_b) < sorted[pivot1])
+// 			do_operation(RB, stk_a, stk_b);
+// 		else
+// 		{
+// 			do_operation(PA, stk_a, stk_b);
+// 			if (get_top(*stk_a) < sorted[pivot2])
+// 				do_operation(RA, stk_a, stk_b);	
+// 		}
+// 	}
+// 	cnt = pivot1 - s;
+// 	while (cnt--)
+// 		do_operation(RRB, stk_a, stk_b);
+// }
+
+// void	b_to_a(t_stack *stk_a, t_stack *stk_b, int s, int e, int *sorted)
+// {
+// 	int cnt;
+// 	int	pivot1;
+// 	int pivot2;
+
+// 	if (e - s == 2)
+// 	{
+// 		// == 3 -> pa pa pa & top_a != 1 -> sa
+// 		// == 2 -> pa & top_b != 3 -> sb & pa sa pa
+// 		// == 1 -> rb pa pa & top_a == 3 -> sa & rrb pa
+// 		if (get_top(*stk_b) == sorted[e])
+// 		{
+// 			do_operation(PA, stk_a, stk_b);
+// 			do_operation(PA, stk_a, stk_b);
+// 			do_operation(PA, stk_a, stk_b);
+// 			if (get_top(*stk_a) != sorted[s])
+// 				do_operation(SA, stk_a, stk_b);
+// 		}
+// 		else if (get_top(*stk_b) == sorted[s])
+// 		{
+// 			do_operation(RB, stk_a, stk_b);
+// 			do_operation(PA, stk_a, stk_b);
+// 			do_operation(PA, stk_a, stk_b);
+// 			if (get_top(*stk_a) == sorted[e])
+// 				do_operation(SA, stk_a, stk_b);
+// 			do_operation(RRB, stk_a, stk_b);
+// 			do_operation(PA, stk_a, stk_b);
+// 		}
+// 		else
+// 		{
+// 			do_operation(PA, stk_a, stk_b);
+// 			if (get_top(*stk_b) != sorted[e])
+// 				do_operation(SB, stk_a, stk_b);
+// 			do_operation(PA, stk_a, stk_b);
+// 			do_operation(SA, stk_a, stk_b);
+// 			do_operation(PA, stk_a, stk_b);
+// 		}
+// 		return ;
+// 	}
+// 	else if (e - s < 2)
+// 	{
+// 		cnt = e - s + 1;
+// 		while (cnt--)
+// 			do_operation(PA, stk_a, stk_b);
+// 		a_to_b(stk_a, stk_b, s, e, sorted);
+// 		return ;
+// 	}
+// 	pivot1 = s + (e - s) / 3;
+// 	pivot2 = e - (e - s) / 3;
+// 	partition_b(stk_a, stk_b, s, e, sorted);
+// 	a_to_b(stk_a, stk_b, pivot2, e, sorted);
+// 	cnt = pivot2 - pivot1;
+// 	while (cnt--)
+// 		do_operation(RRA, stk_a, stk_b);
+// 	a_to_b(stk_a, stk_b, pivot1, pivot2 - 1, sorted);
+// 	b_to_a(stk_a, stk_b, s, pivot1 - 1, sorted);
+// }
 
 /* 
 
