76cac719e3474eec13f0bf787207a06ad9f97557
Heeoh Son <heson@cx1r2s1.42seoul.kr>
2023-02-22 20:33:41 +0900

    player move

--- so_long/headers/so_long_type.h
+++ so_long/headers/so_long_type.h
@@ -6,7 +6,7 @@
 /*   By: heson <heson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 21:29:18 by heson             #+#    #+#             */
-/*   Updated: 2023/02/21 16:35:14 by heson            ###   ########.fr       */
+/*   Updated: 2023/02/22 19:38:27 by heson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,21 @@ enum e_type {
 	PLAYER = 'P'
 };
 
+enum e_keycode {
+	KEY_UP = 13,
+	KEY_DOWN = 1,
+	KEY_LEFT = 0,
+	KEY_RIGHT = 2,
+	KEY_ESC = 53
+};
+
+enum e_direction {
+	UP = 0,
+	RIGHT,
+	DOWN,
+	LEFT
+};
+
 typedef struct s_point {
 	int	x;
 	int	y;
@@ -38,11 +53,19 @@ typedef struct s_map {
 	int	height;
 }		t_map;
 
+typedef struct s_mlx_vars {
+	void	*mlx;
+	void	*win;
+}	t_mlx_vars;
+
+
 typedef struct s_game
 {
-	t_map	map;
-	t_point	player;
-	int		item_cnt;
+	t_mlx_vars	vars;
+	t_map		map;
+	t_point		player;
+	int			item_cnt;
+	int			move_cnt;
 }	t_game;
 
 #endif
--- so_long/sources/so_long.c
+++ so_long/sources/so_long.c
@@ -6,7 +6,7 @@
 /*   By: heson <heson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 15:52:42 by heson             #+#    #+#             */
-/*   Updated: 2023/02/22 17:37:15 by heson            ###   ########.fr       */
+/*   Updated: 2023/02/22 20:33:18 by heson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,68 +16,114 @@
 #include <stdio.h>
 
 
-int close(t_vars *vars)
+int close(t_mlx_vars *vars)
 {
 	ft_printf("close\n");
 	mlx_destroy_window(vars->mlx, vars->win);
 	exit(0);
 }
 
-int resize(t_vars *vars)
+// int resize(t_game *g)
+// {
+// 	g->vars->win = vars->win;
+// 	ft_printf("resize\n");
+// 	return (0);
+// }
+void	move(t_game *game, int dir)
 {
-	vars->win = vars->win;
-	ft_printf("resize\n");
+	int yy[4] = {-1, 0, 1, 0};
+	int xx[4] = {0, 1, 0, -1};
+	t_point next;
+
+	next.y = game->player.y + yy[dir];
+	next.x = game->player.x + xx[dir];
+	if (game->map.map[next.y][next.x] == WALL) {
+		ft_printf("WALL, cannot move\n");
+		return ;
+	}
+	else if (game->map.map[next.y][next.x] == ITEM)
+	{
+		ft_printf("ITEM, get item and ");
+		game->item_cnt--;
+		game->map.map[next.y][next.x] = EMPTY;
+	}
+	else if (game->map.map[next.y][next.x] == EXIT)
+	{
+		if (game->item_cnt == 0) {
+			ft_printf("EXIT, game finish\n");
+			close(&(game->vars));
+		}
+	}
+	game->player.y = next.y;
+	game->player.x = next.x;
+	game->move_cnt++;
+	ft_printf("move to (%d, %d)\n", game->player.y, game->player.x);
+}
+
+int	key_hook(int keycode, t_game *g)
+{
+	// printf("%d\n", keycode);
+	if (keycode == KEY_UP)
+		move(g, UP);
+	else if (keycode == KEY_DOWN)
+		move(g, DOWN);
+	else if (keycode == KEY_LEFT)
+		move(g, LEFT);
+	else if (keycode == KEY_RIGHT)
+		move(g, RIGHT);
+	else if (keycode == KEY_ESC) {
+		ft_printf("ESC\n");
+		close(&(g->vars));
+	}
 	return (0);
 }
 
-int	key_hook(int keycode, t_vars *vars)
+void	find_player(t_map *m, t_point *p)
 {
-	// printf("%d\n", keycode);
-	if (keycode == 13)
-		ft_printf("up\n");
-	else if (keycode == 1)
-		ft_printf("down\n");
-	else if (keycode == 0)
-		ft_printf("left\n");
-	else if (keycode == 2)
-		ft_printf("right\n");
-	else if (keycode == 53) {
-		ft_printf("ESC\n");
-		close(vars);
+	p->y = -1;
+	while (++p->y < m->height)
+	{
+		p->x = -1;
+		while (++p->x < m->width)
+		{
+			if (m->map[p->y][p->x] == PLAYER)
+				return ;
+		}
 	}
-	return (0);
 }
 
 void	init(char *file, t_game *game)
 {
 	if (!map_parsing(file, &(game->map), &(game->item_cnt)))
+	{
 		printf("Error\n");
-	else {
-		for (int i=0; i < game->map.height; i++) {
-			for (int j=0; j < game->map.width; j++)
-				printf("%c ", game->map.map[i][j]);
-			printf("\n");
-		}
+		return ;
 	}
-
-
+	// for (int i=0; i < game->map.height; i++) {
+	// 	for (int j=0; j < game->map.width; j++)
+	// 		printf("%c ", game->map.map[i][j]);
+	// 	printf("\n");
+	// }
+	find_player(&(game->map), &(game->player));
+	game->map.map[game->player.y][game->player.x] = EMPTY;
+	game->move_cnt = 0;
+	game->vars.mlx = mlx_init();
+	game->vars.win = mlx_new_window(game->vars.mlx, 640, 480, "test");
+	// ft_printf("player at (%d, %d)\n", game->player.y, game->player.x);
 }
+
 
 int	main(int ac, char *av[])
 {
-	// t_vars	vars;
-
-	// vars.mlx = mlx_init();
-	// vars.win = mlx_new_window(vars.mlx, 640, 480, "test");
-	// // mlx_key_hook(vars.win, key_hook, &vars);
-	// mlx_hook(vars.win, ON_KEYDOWN, 1L<<0, key_hook, &vars);
-	// mlx_hook(vars.win, ON_DESTROY, 0, close, &vars);
-	// mlx_hook(vars.win, 25, 1L<<18, resize, &vars);
-	// mlx_loop(vars.mlx);
-
 	t_game	game;
 
 	if (ac != 2)
 		return (0);
 	init(av[1], &game);
+
+	// mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_hook(game.vars.win, ON_KEYDOWN, 1L<<0, key_hook, &game);
+	mlx_hook(game.vars.win, ON_DESTROY, 0, close, &game);
+	mlx_loop(game.vars.mlx);
+
 }
