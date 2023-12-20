72fea0ab28e9d3a460fb0610a941c01bbb96c855
heeoh <hio4662@naver.com>
2023-12-20 15:38:23 +0900

    done inception

---
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,25 @@
+#!bin/sh
+
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	# init database
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	tfile='mktemp'
+	cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* to '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
+	rm -f $tfile
+fi
+
+exec /usr/bin/mysqld --user=mysql $@
